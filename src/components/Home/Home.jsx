f152f4fcdecd5ba28a9858fa06eec8039fb443b0
Youssef Halleb <161064366+YoussefHalleb@users.noreply.github.com>
2025-07-30 14:26:02 +0200

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     if (!email) return;
 
     try {
-      const res = await fetch('http://backend.duomingo.ddns.net/subscribe', {
+      const res = await fetch('http://backend.duomingo.ddns.net', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
